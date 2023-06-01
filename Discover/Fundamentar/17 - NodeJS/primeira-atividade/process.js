8157f576d18df3eea3ddfcfb654c9e8f0d4c8563
Rique25 <luiz.martendal52@gmail.com>
2023-06-01 17:52:31 -0300

    .

---
+++ Discover/Fundamentar/17 - NodeJS/primeira-atividade/process.js
@@ -0,0 +1,4 @@
+console.log(process)
+console.log(process.argv)
+
+console.log('Meu nome é ', process.argv[2] + ' ' + process.argv[3] + ' ' + process.argv[4])
---
+++ Discover/Fundamentar/17 - NodeJS/primeira-atividade/processValue.js
@@ -0,0 +1,3 @@
+const getFlagValue = require('./flagValue')
+
+console.log(`${getFlagValue('--greating')} ${getFlagValue('--name')}!`)
